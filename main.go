441a0a48ffeb0741459ea622f9f87088d5633964
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-10-28 04:12:13 +0000

    Generate plot

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "gosl/io"
+import (
+	"gosl/plt"
+	"gosl/utl"
+	"math"
+)
 
 func main() {
-	io.Pf("Hello Gosl (6031)!!!\n")
-	for i, n := range []int{6, 0, 3, 1} {
-		io.Pf("%d: the number is now = %d\n", i, n)
+
+	// grid size
+	xmin, xmax, N := -math.Pi/2.0+0.1, math.Pi/2.0-0.1, 21
+
+	// mesh grid
+	X, Y, F := utl.MeshGrid2dF(xmin, xmax, xmin, xmax, N, N, func(x, y float64) (z float64) {
+		m := math.Pow(math.Cos(x), 2.0) + math.Pow(math.Cos(y), 2.0)
+		z = -math.Pow(m, 2.0)
+		return
+	})
+
+	// configuration
+	a := &plt.A{
+		NumFmt:  "%.1f",
+		Lw:      0.8,
+		CbarLbl: "$f(x,y)$",
+		SelectC: "yellow",
+		SelectV: -2.5,
+		Nlevels: 10,
 	}
+
+	plt.Reset(true, nil)
+	plt.Equal()
+	plt.ContourF(X, Y, F, a)
+	plt.SetLabels("$x$", "$y$", nil)
+
+	plt.Save("output", "myplot")
 }
